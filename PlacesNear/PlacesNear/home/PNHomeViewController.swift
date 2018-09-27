c4d785a926f482fc6490ff45665a03ed9e14b502
apple <apple@appledemacbook-pro.local>
2018-09-27 10:13:25 +0800

    创建项目

---
+++ PlacesNear/PlacesNear/home/PNHomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PNHomeViewController.swift
+//  PlacesNear
+//
+//  Created by apple on 2018/9/27.
+//  Copyright © 2018年 zsx. All rights reserved.
+//
+
+import UIKit
+
+class PNHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.orange
+        
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        print("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
