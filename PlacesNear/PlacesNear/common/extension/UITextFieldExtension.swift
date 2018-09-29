b6ada6784d7eb4c1087471720878b4bf2c366f04
ChenSZ <zsx@192.168.0.104>
2018-09-29 22:26:28 +0800

    添加分类

---
+++ PlacesNear/PlacesNear/common/extension/UITextFieldExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UITextFieldExtension.swift
+//  PlacesNear
+//
+//  Created by zsx on 2018/9/29.
+//  Copyright © 2018年 zsx. All rights reserved.
+//
+
+import Foundation
+
+extension UITextField {
+    func addTopBar() {
+        let btn = UIButton(type: UIButtonType.custom)
+        btn.setTitle("完成", for: UIControlState.normal)
+        btn.titleLabel?.font = FontSize16
+        btn.backgroundColor = UIColor.black
+        btn.frame = CGRect(x: 0, y: 0, width: KScreenWidth, height: 44)
+        btn.addTarget(self, action: #selector(closeKeyboardAction), for: UIControlEvents.touchUpInside)
+        
+        self.inputAccessoryView = btn
+    }
+    @objc private func closeKeyboardAction() {
+        self.resignFirstResponder()
+    }
+}
